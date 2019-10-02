2c41185ba2b72596756c735a05267ec637d1bad3
batriRahul <55807435+batriRahul@users.noreply.github.com>
2019-10-02 12:37:32 +0530

    removed unnecessary libraries

--- app/src/main/java/com/example/magnumintern/MainActivity.java
+++ app/src/main/java/com/example/magnumintern/MainActivity.java
@@ -12,14 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
